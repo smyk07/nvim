399571cc52feb44d5b162270ed6642118f1b8939
smyk07 <samyak.bambole07@gmail.com>
2024-07-22 22:52:31 +0530

    updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     -- import/override with your plugins
--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -37,7 +37,7 @@ return {
     dashboard.section.header.opts.hl = "AlphaHeader"
     dashboard.section.buttons.opts.hl = "AlphaButtons"
     dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.opts.layout[1].val = 6
+    dashboard.opts.layout[1].val = 12
     return dashboard
   end,
   config = function(_, dashboard)
